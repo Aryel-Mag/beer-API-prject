b4462220ceed05bf4b2ef3266b31a0a8c4ab955d
Ariel Victorio Magnane <magsmacbook@AMP.local>
2023-07-07 17:06:00 +0200

    selectors working, can't access state.data from object

--- src/app/store/beers.selector.ts
+++ src/app/store/beers.selector.ts
@@ -2,11 +2,12 @@ import { createFeatureSelector, createSelector, MemoizedSelector } from '@ngrx/s
 import { AppState, IPayloadStatus, IBeer } from '../interfaces/beerInterface';
 
 export const selectBeersState = createFeatureSelector<AppState>('beers');
+console.log('selector beers', selectBeersState);
 
 export const selectAllBeers = createSelector(
   selectBeersState,
   (state: AppState) => {
-    console.log(state.beers);
+    console.log(state);
     return state.beers ? state.beers.data : [];
   }
 );
